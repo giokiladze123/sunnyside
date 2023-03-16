19d14780e0ecd949709fe5136da5895f195cbb8d
giorgi kiladze <giorgikiladze92@gmail.com>
2023-03-16 07:52:00 -0700

    sunnyside

---
+++ index.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger")
+const menu = document.querySelector(".menuconteiner")
+
+hamburger.addEventListener("click",function(){
+  menu.classList.toggle("hidden")
+
+}) 
